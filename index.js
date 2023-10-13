30da9b1373fd60b48a5e41c89978a6cf56983296
Shelli Martinez <shelli.mrtnz@gmail.com>
2023-10-13 13:22:59 -0400

    add completed lab

--- index.js
+++ index.js
@@ -9,3 +9,11 @@ function introductionWithLanguage(name, language) {
 function introductionWithLanguageOptional(name, language = 'JavaScript') {
     return `Hi, my name is ${name} and I am learning to program in ${language}.`
 }
+
+function introductionWithLanguage(name, language) {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+}
+
+function introductionWithLanguagOptional(name, language = 'JavaScript') {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+}
